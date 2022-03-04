adc2d883547c8b7939a7cd7c3de23958dff7eb1b
raihan <rnavroze@gmail.com>
2022-03-05 04:24:22 +0530

    Add definitons and more features

--- IRSend.ino
+++ IRSend.ino
@@ -11,7 +11,7 @@
 
 IRsend My_Sender;
 unsigned int counter = 10;
-unsigned int* ptr = &temp25c[0];
+unsigned int* ptr = &AC_TEMP25C[0];
 unsigned int currentData[111];
 bool flag = true;
    
@@ -22,26 +22,49 @@ void setup()
 
 void loop() {
   if (Serial.available() > 0) {
-      while (Serial.available() > 0) Serial.read();
-      Serial.println("got input");
+    String input = Serial.readString();
+      Serial.println("got input" + input);
       
-      unsigned int displayInt;
-      if (flag) {
-        Serial.println("19c");
-        ptr = &temp19c[0];
-            for (byte k = 0; k < 111; k++) {
-              displayInt = pgm_read_word_near(ptr + k);
-              currentData[k] = displayInt;
-            }
+      if (input == "on\n") {
+        setCurrentData(&AC_ONOFFA[0]);
+      } else if (input == "off\n") {
+        setCurrentData(&AC_ONOFFB[0]);
+      } else if (input == "mode\n") {
+        setCurrentData(&AC_MODE[0]);
+      } else if (input == "turbo\n") {
+        setCurrentData(&AC_TURBO[0]);
+      } else if (input == "swing\n") {
+        setCurrentData(&AC_SWINGON[0]);
+      } else if (input == "16\n") {
+        setCurrentData(&AC_TEMP16C[0]);
+      } else if (input == "17\n") {
+        setCurrentData(&AC_TEMP17C[0]);
+      } else if (input == "18\n") {
+        setCurrentData(&AC_TEMP18C[0]);
+      } else if (input == "19\n") {
+        setCurrentData(&AC_TEMP19C[0]);
+      } else if (input == "20\n") {
+        setCurrentData(&AC_TEMP20C[0]);
+      } else if (input == "21\n") {
+        setCurrentData(&AC_TEMP21C[0]);
+      } else if (input == "22\n") {
+        setCurrentData(&AC_TEMP22C[0]);
+      } else if (input == "23\n") {
+        setCurrentData(&AC_TEMP24C[0]); // TODO
+      } else if (input == "24\n") {
+        setCurrentData(&AC_TEMP24C[0]);
+      } else if (input == "25\n") {
+        setCurrentData(&AC_TEMP25C[0]);
+      } else if (input == "26\n") {
+        setCurrentData(&AC_TEMP26C[0]);
+      } else if (input == "27\n") {
+        setCurrentData(&AC_TEMP27C[0]);
+      } else if (input == "28\n") {
+        setCurrentData(&AC_TEMP28C[0]);
       } else {
-        Serial.println("25c");
-        ptr = &temp25c[0];
-            for (byte k = 0; k < 111; k++) {
-              displayInt = pgm_read_word_near(ptr + k);
-              currentData[k] = displayInt;
-            }
+        Serial.println("Unknown input"); 
       }
-      flag = !flag;
+       
   }
      
   My_Sender.IRsendRaw::send(currentData, 111, 38);
@@ -51,5 +74,14 @@ void loop() {
     delay(2000);
     Serial.print(".");
     counter = 10;
+  }
+}
+
+void setCurrentData(unsigned int* signalCode) {
+  unsigned int displayInt;
+  
+  for (byte k = 0; k < SIGNAL_SIZE; k++) {
+    displayInt = pgm_read_word_near(signalCode + k);
+    currentData[k] = displayInt;
   }
 }
